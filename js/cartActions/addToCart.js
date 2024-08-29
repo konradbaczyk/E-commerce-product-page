c68d4dac81526f9f1f522e76ed55096f47f4d6d8
Konrad01~ <konradbc7@gmail.com>
2024-08-29 22:51:59 +0200

    Implement add to cart logic. Items in cart is working - without cart view

---
+++ js/cartActions/addToCart.js
@@ -0,0 +1,16 @@
+import { cartBtnCounter, productCounterNumber } from "../scirpt.js";
+
+export const incrementQuantityToAdd = () => {
+	productCounterNumber.innerText++;
+};
+
+export const decrementQuantityToAdd = () => {
+	if (productCounterNumber.innerText >= 1) {
+		productCounterNumber.innerText--;
+	}
+};
+
+export const addToCart = () => {
+	cartBtnCounter.innerText = Number(cartBtnCounter.innerText) + Number(productCounterNumber.innerText);
+	cartBtnCounter.classList.add("navbar__cart-btn--added-items-active");
+};
--- js/scirpt.js
+++ js/scirpt.js
@@ -1,6 +1,7 @@
 import { checkLinksVisibility } from "./menuActions/checkLinksVis.js";
 import { handleMenu } from "./menuActions/handleMobileMenu.js";
 import { checkThumbnailClick } from "./imagesActions/showBigImg.js";
+import { incrementQuantityToAdd, decrementQuantityToAdd, addToCart } from "./cartActions/addToCart.js";
 
 // Menu items
 export const btnToggle = document.getElementById("btn-toggle-menu");
@@ -11,6 +12,13 @@ export const navLinks = document.querySelectorAll(".navbar__menu--link");
 export const images = document.querySelectorAll(".images__big-image");
 const thumbnailsImagesBtns = document.querySelectorAll(".images__thumbnail-btn");
 
+// Add to cart items
+export const cartBtnCounter = document.getElementById("cart-btn-counter");
+export const productCounterNumber = document.getElementById("add-to-cart-counter-number");
+const productCounterMinusBtn = document.getElementById("add-to-cart-counter-minus");
+const productCounterPlusBtn = document.getElementById("add-to-cart-counter-plus");
+const addToCartBtn = document.getElementById("add-to-cart-btn");
+
 // Menu actions
 btnToggle.addEventListener("click", handleMenu);
 window.addEventListener("DOMContentLoaded", checkLinksVisibility);
@@ -18,3 +26,8 @@ window.addEventListener("resize", checkLinksVisibility);
 
 // Images actions
 thumbnailsImagesBtns.forEach((thumbnailButton) => thumbnailButton.addEventListener("click", checkThumbnailClick));
+
+// Add to cart actions
+productCounterPlusBtn.addEventListener("click", incrementQuantityToAdd);
+productCounterMinusBtn.addEventListener("click", decrementQuantityToAdd);
+addToCartBtn.addEventListener("click", addToCart);
